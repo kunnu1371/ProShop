7604867b3514a4cc042b7f0d0e3a94d01ff24c16
Kunal Gautam <kunalgautam1371@gmail.com>
2021-06-02 13:45:52 +0530

    cart operations done

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,12 +5,20 @@ import {
   productListReducer,
   productDetailsReducer,
 } from "../src/reducers/productReducer";
+import { cartReducer } from "./reducers/cartReducer";
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 });
-const initialState = {};
+
+const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+
+const initialState = {
+  cart: {cartItems: cartItemsFromStorage}
+}
+
 const middleware = [thunk];
 const store = createStore(
   reducer,
