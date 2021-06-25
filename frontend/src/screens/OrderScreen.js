938c988a26053270fe238b7a1ac5bf70d9924dc4
Kunal Gautam <kunalgautam1371@gmail.com>
2021-06-25 20:59:51 +0530

    -

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -1,11 +1,18 @@
-import React, { useState, useEffect } from "react";
+ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Loader from "../components/Loader.js";
 import Message from "../components/Message.js";
 import { Button, Row, Col, ListGroup, Image, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { getOrderDetails, payOrder, deliverOrder } from "../actions/orderActions.js";
-import { ORDER_PAY_RESET, ORDER_DELIVER_RESET } from "../constants/orderContants.js";
+import {
+  getOrderDetails,
+  payOrder,
+  deliverOrder,
+} from "../actions/orderActions.js";
+import {
+  ORDER_PAY_RESET,
+  ORDER_DELIVER_RESET,
+} from "../constants/orderContants.js";
 import axios from "axios";
 import { PayPalButton } from "react-paypal-button-v2";
 
@@ -55,9 +62,9 @@ const OrderScreen = ({ match, history }) => {
       document.body.appendChild(script);
     };
 
-    if (!order || successPay || successDeliver) {
+    if (!order || successPay || successDeliver || order._id !== orderId) {
       dispatch({ type: ORDER_PAY_RESET });
-      dispatch({ type: ORDER_DELIVER_RESET })
+      dispatch({ type: ORDER_DELIVER_RESET });
       dispatch(getOrderDetails(orderId));
     } else if (!order.isPaid) {
       if (!window.paypal) {
@@ -73,9 +80,9 @@ const OrderScreen = ({ match, history }) => {
     dispatch(payOrder(orderId, paymentResult));
   };
 
-const deliverHandler = () => {
-  dispatch(deliverOrder(order))
-}
+  const deliverHandler = () => {
+    dispatch(deliverOrder(order));
+  };
 
   return loading ? (
     <Loader />
@@ -147,7 +154,8 @@ const deliverHandler = () => {
                           </Link>
                         </Col>
                         <Col md={4}>
-                          {item.qty} x ${item.price} = ${(item.qty * item.price).toFixed(2)}
+                          {item.qty} x ${item.price} = $
+                          {(item.qty * item.price).toFixed(2)}
                         </Col>
                       </Row>
                     </ListGroup.Item>
@@ -224,3 +232,4 @@ const deliverHandler = () => {
 };
 
 export default OrderScreen;
+
